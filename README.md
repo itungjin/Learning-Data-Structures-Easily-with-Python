8f454b984d2a03d3facf82eacd52affb9883fdc7
nnoobbaagguu <nnoobbaagguu@naver.com>
2023-07-25 20:12:19 +0900

    230725

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Learning-Data-Structures-Easily-with-Python
 
-쉽게 배우는 자료구조 with 파이썬
+쉽게 배우는 자료구조 with 파이썬 공부
 
 ## 오타
 
@@ -16,3 +16,11 @@ Heap.counter를 Test.counter로 변경
 ### 110쪽 알고리즘 5-3 if문 첫번째 줄
 
 **head.next = **head.next.next를 **head = **head.next로 변경
+
+### 122쪽 코드 5-14 제목
+
+연결 리스트 뒤집기를 연결 리스트 정렬하기로 변경
+
+### 128쪽 아래서 두번째 줄
+
+비효휼성을 비효율성으로 변경
--- ch05/linked_list_basic.py
+++ ch05/linked_list_basic.py
@@ -1,4 +1,4 @@
-import ListNode
+from list_node import ListNode
 
 
 class LinkedListBasic:
@@ -37,7 +37,7 @@ class LinkedListBasic:
         else:
             return None
 
-    def __find_node(self, x) -> tuple(ListNode, ListNode):
+    def __find_node(self, x) -> tuple([ListNode, ListNode]):
         prev = self.__head
         curr = prev.next
         while curr != None:
@@ -53,11 +53,14 @@ class LinkedListBasic:
         if curr != None:
             prev.next = curr.next
             self.__num_items -= 1
+            return x
+        else:
+            return None
 
     def get(self, i: int):
         if self.is_empty():
             return None
-        elif 0 <= i < self.__num_items:
+        elif 0 <= i <= self.__num_items - 1:
             return self.__get_node(i).item
         else:
             return None
@@ -76,3 +79,57 @@ class LinkedListBasic:
 
     def size(self) -> int:
         return self.__num_items
+
+    def clear(self):
+        self.__head = ListNode("dummy", None)
+        self.__num_items = 0
+
+    def count(self, x) -> int:
+        cnt = 0
+        curr = self.__head.next
+        while curr != None:
+            if curr.item == x:
+                cnt += 1
+            curr = curr.next
+        return cnt
+
+    def extend(self, a):
+        for index in range(a.size()):
+            self.append(a.get(index))
+
+    def copy(self):
+        new_linked_list = LinkedListBasic()
+        for index in range(self.__num_items):
+            new_linked_list.append(self.get(index))
+        return new_linked_list
+
+        # new_linked_list = LinkedListBasic()
+        # new_linked_list.extend(self)
+        # return new_linked_list
+
+    def reverse(self):
+        temp_linked_list = LinkedListBasic()
+        for index in range(self.__num_items):
+            temp_linked_list.insert(0, self.get(index))
+
+        # temp_linked_list = self.copy()
+
+        self.clear()
+        for index in range(temp_linked_list.size()):
+            self.append(temp_linked_list.get(index))
+
+    def sort(self) -> None:
+        temp_list = []
+        for index in range(self.__num_items):
+            temp_list.append(self.get(index))
+        temp_list.sort()
+        self.clear()
+        for item in temp_list:
+            self.append(item)
+
+    def print_list(self):
+        curr = self.__head.next
+        while curr != None:
+            print(curr.item, end=' ')
+            curr = curr.next
+        print()
